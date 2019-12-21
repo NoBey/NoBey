2357eeb356cf7993b8103c7d56e0feaa10f08101
NoBey <nobey@nobey.cn>
2019-12-21 16:20:03 +0800

    t

---
+++ config.js
@@ -0,0 +1,15 @@
+var _config = {
+    blog_name       : 'NoBey',         // 博客名称
+    owner           : 'NoBey',           // github 用户名
+    repo            : 'nobey',// github 中对应项目名
+    per_page        : '10',                    // 默认一页显示几篇文章
+    // 版权起始年份
+    since_year: 2015,
+    // icp备案号 ICP_license: 京ICP备1234556号-1
+    ICP_license: false,
+    // 设置为 true 发布后将使用 unpkg cdn
+    cdn: false,
+    // gitalk 配置
+    client_id: '1e401efd17c830cfcfe2',
+    client_secret: '7f8e1108070d5faa43bf4823f7077f229910d419',
+}
